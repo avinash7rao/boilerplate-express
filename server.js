39b1f74b30153392645c66690eb4d8fb6ab8a378
Avinash Rao <avinash7rao@gmail.com>
2025-02-11 23:48:46 +0000

    .

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ const app = express();
 
 
 if (!process.env.DISABLE_XORIGIN) {
-  app.get("/", (req, res) => {
-    res.send("Hello Express");
+  app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
   });
   app.use((req, res, next) => {
     const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
