d40211543079587ff00dd5d4c5cce640b8e7380c
Avinash Rao <avinash7rao@gmail.com>
2025-02-11 23:40:56 +0000

    .

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const app = express();
 
 
 if (!process.env.DISABLE_XORIGIN) {
-  app.get("/", (req, res) => {
-    res.send("Hello Express");
-  });
 
   app.use((req, res, next) => {
     const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
     const origin = req.headers.origin || '*';
+    app.get("/", (req, res) => {
+      res.send("Hello Express");
+    });
     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
          console.log(origin);
          res.setHeader('Access-Control-Allow-Origin', origin);
